c7d89cb547454624dbbd9d89ccef0508678cf0f0
fitronansori <fitronansori7@gmail.com>
2024-08-23 15:39:45 +0800

    refactor: Update dashboard layout styles for responsive design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,10 +8,10 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="flex size-full">
-      <div className="hidden lg:block w-[20%] h-screen">
+      <div className="hidden lg:block lg:w-80 h-screen fixed left-0 top-0">
         <Sidebar />
       </div>
-      <div className="w-full lg:w-[80%]">
+      <div className="w-full lg:ml-80">
         <Header />
         {children}
       </div>
