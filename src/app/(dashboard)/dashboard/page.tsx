e4f63452934019573245bd7687f61836a4415f25
fitronansori <fitronansori7@gmail.com>
2024-08-23 15:40:15 +0800

    feat: Add search functionality

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,4 +1,20 @@
+"use client";
+
+import { useState } from "react";
+import Templates from "../_components/Templates";
+import TemplateSearching from "../_components/Templates/TemplateSearching";
+
 const Dashboard = () => {
-  return <section>Dashboard</section>;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (query: string) => {
+    setSearchQuery(query);
+  };
+  return (
+    <section>
+      <TemplateSearching onSearchChange={handleSearchChange} />
+      <Templates searchQuery={searchQuery} />
+    </section>
+  );
 };
 export default Dashboard;
